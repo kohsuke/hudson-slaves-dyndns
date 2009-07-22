3bf99dbffa3ec877136bc3c93043da536af1b0a6
Kohsuke Kawaguchi <kk@kohsuke.org>
2009-07-22 16:23:11 -0700

    initial version

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+# based on http://tldp.org/HOWTO/html_single/Debian-Binary-Package-Building-HOWTO/
+
+name=hudson-slaves-dyndns
+version=1.0
+
+mkdir DEBIAN
+cat > DEBIAN/control << EOF
+Package: $name
+Version: $version
+Section: base
+Priority: optional
+Architecture: all
+Depends: coreutils
+Maintainer: Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
+Description: Automatic DNS update script for Hudson slaves
+EOF
+
+# build a package
+pkg=${name}_${version}_all.deb
+fakeroot dpkg-deb --build debian $pkg
+cp $pkg ~/debian-repository/binary
+
